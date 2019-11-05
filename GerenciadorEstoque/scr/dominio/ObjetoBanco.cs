03b91e55162b2db5908fc98bea8823ed94d1c35f
Emanuel Passinato <emanuelborges123@hotmail.com>
2019-11-04 22:47:58 -0300

    commi criando Produto, Objeto Banco e String utils

---
+++ GerenciadorEstoque/scr/dominio/ObjetoBanco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorEstoque.src.dominio {
+    abstract class ObjetoBanco {
+        public long Id { get; set; }
+
+        public long GetId(ObjetoBanco value) {
+            if (value == null) {
+                return 0;
+            } else {
+                return value.Id;
+            }
+        }
+    }
+}
