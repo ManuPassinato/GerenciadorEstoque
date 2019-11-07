d108f1ed46cfc65d5cfdfd22fd330d17674609da
Felipe Silveira Schloegl <schloegl10@hotmail.com>
2019-11-07 20:16:02 -0300

    Conflitos

--- GerenciadorEstoque/scr/dominio/Compra.cs
+++ GerenciadorEstoque/scr/dominio/Compra.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using GerenciadorEstoque.src.utils;
 
 
-namespace GerenciadorEstoque.src.dominio {
-    class Compra : ObjetoBanco {
+namespace GerenciadorEstoque.src.dominio
+{
+    class Compra : ObjetoBanco
+    {
 
         public long Codigo { get; set; }
         public Fornecedor Fornecedor { get; set; }
         public List<Unidade> MateriasPrimasUsadas { get; set; }
-		public List<Unidade> produtosComprados { get; set; }
-		public Decimal valorTotal { get; }
-		public DateTime data { get; set; }
+        public List<Unidade> produtosComprados { get; set; }
+        public Decimal valorTotal { get; }
+        public DateTime data { get; set; }
     }
-
 }
